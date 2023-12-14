aa94e0586bb9847057649304e7733bdadb601db6
Mark Ivan Hernandez <m.hernandez@prostrive.io>
2023-12-14 16:53:23 +0800

    adjusted the tablet view

--- src/component/00_header/desktop/DesktopHeader.jsx
+++ src/component/00_header/desktop/DesktopHeader.jsx
@@ -5,9 +5,9 @@ import LanguageSwitcher from "../LanguageSwitcher"
 const DesktopHeader = ({setCountryCode, navigations, countryCode }) => {
   if(navigations){
     return (
-      <div className="hidden max-w-6xl w-full lg:flex lg:justify-between lg:items-center lg:px-8 ">
+      <div className="hidden max-w-6xl w-full md:flex md:justify-between md:items-center lg:px-8 ">
         <RenderLogo logoMarkStyles="w-16 h-6 lg:w-[119.955px] lg:h-[60.352px]" logoTextStyles="h-4 w-36 lg:w-[270px] lg:h-[28.421px]"/>
-        <div className="flex gap-8 items-center text-base">
+        <div className="flex gap-8 md:gap-5 items-center text-base">
               {navigations.map(({name, href}, index)=>(
                 <Link href={href} key={index}><span className="rounded-lg text-lg text-gray-700 hover:text-green" >{name}</span></Link>
               ))}
