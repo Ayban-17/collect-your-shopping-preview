3c3a5b122982fee726fb9483508a4f88a0e74cf5
Mark Ivan Hernandez <m.hernandez@prostrive.io>
2023-12-14 16:10:53 +0800

    star assets changed

--- src/component/03_awards/Awards.jsx
+++ src/component/03_awards/Awards.jsx
@@ -6,7 +6,7 @@ const Awards = ({starsText, mobileUxAwardText, featuredByAppleText}) => {
       <Image src="/assets/03_awards/bg_awards.png" alt="gifs" height={600} width={500} className="w-full"/>
         <div className="max-w-[1440px] flex justify-center gap-8 px-6 -mt-10 lg:justify-center lg:gap-44 lg:w-full lg:-mt-36">
           <div className="flex flex-col  w-32 items-center lg:w-44 ">
-            <Image src="/assets/03_awards/stars.gif" alt="gifs" height={600} width={500} className="w-20 h-10 lg:w-40 lg:h-20"/>
+            <Image src="/assets/03_awards/Stars.gif" alt="gifs" height={600} width={500} className="w-20 h-10 lg:w-40 lg:h-20"/>
             <p className="text-[10px] font-medium text-center lg:text-[19px] ">{starsText}</p>
           </div>
           <div className="flex flex-col  w-32 items-center lg:w-44 ">
